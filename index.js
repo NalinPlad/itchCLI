d3cd5689341be5a55ba439c8949d9ef6d283c8cd
NalinPlad <43052612+NalinPlad@users.noreply.github.com>
2022-04-20 12:05:09 -0700

    bruh

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 const childProcess = require("child_process");
 const fs = require("fs");
 const os = require("os");
+const path_mod = require("path");
 const { AutoComplete } = require("enquirer");
+
+
+const apps_path = os.homedir + "/Library/Application Support/itch/apps/";
 
 function getDirectories(path) {
   return fs.readdirSync(path).filter(function (file) {
@@ -9,16 +13,20 @@ function getDirectories(path) {
   });
 }
 
-app_dirs = getDirectories(
-  os.homedir + "/Library/Application Support/itch/apps/"
-);
+function getApps(path) {
+  return fs.readdirSync(path).filter(function (file) {
+    return path_mod.extname(path + "/" + file) == ".app";
+  });
+}
+
+app_dirs = getDirectories(apps_path);
 app_names = app_dirs.map((e) => e.replace(/-/g, " "));
 
 const prompt = new AutoComplete({
   name: "app",
   message: "Launch",
   limit: 10,
-  initial: 2,
+  initial: 1,
   choices: app_names,
 });
 
@@ -27,16 +35,20 @@ app_choice = [];
 prompt
   .run()
   .then((answer) => {
+    console.log("Launching .. 🚀");
+
+    launch_dir =
+      apps_path + getApps(apps_path + answer.replace(/ /g, "-"))[0] + "/";
+
+    console.log(launch_dir);
     
-    console.log(answer);
+    childProcess.exec(launch_dir, function (err, stdout, stderr) {
+            if (err) {
+            console.error(err);
+            return;
+        }
+        console.log(stdout);
+        process.exit(0);// exit process once it is opened
+})
   })
   .catch(console.error);
-
-// childProcess.exec('start Example.xlsx', function (err, stdout, stderr) {
-//         if (err) {
-//         console.error(err);
-//         return;
-//     }
-//     console.log(stdout);
-//     process.exit(0);// exit process once it is opened
-// })
